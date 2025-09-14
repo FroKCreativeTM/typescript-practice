ff0e928f29e26a820303c3cfd5629e7c161fc420
FrokCreativeTM <cscculture95@gmail.com>
2025-09-14 23:01:07 +0900

    컨트롤러에 서비스 추가 및 테스트

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,18 +1,33 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto'
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    messagesService: MessagesService;
+
+    constructor() {
+        // 컨트롤러는 서비스를 주입받는다.
+        // 아래는 임시 코드!
+        this.messagesService = new MessagesService();
+    }
+
     @Get()
-    listMessages() { }
+    listMessages() { 
+        return this.messagesService.findAll();
+    }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto) { 
         console.log(body);
+
+        this.messagesService.create(body.content);
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string) { 
         console.log(id);
+
+        return this.messagesService.findOne(id);
     }
 }
--- messages/src/messages/messages.service.ts
+++ messages/src/messages/messages.service.ts
@@ -1,3 +1,23 @@
-export class MessageService {
-    
+import { MessagesRepository } from "./messages.repository";
+
+export class MessagesService {
+    messagesRepository: MessagesRepository;
+
+    constructor() {
+        // 서비스는 레포지토리를 주입받는다.
+        // 아래는 임시 코드!
+        this.messagesRepository = new MessagesRepository();
+    }
+
+    async findOne(id: string) {
+        return this.messagesRepository.findOne(id);
+    }
+
+    async findAll() {
+        return this.messagesRepository.findAll();
+    }
+
+    async create(message: string) {
+        return this.messagesRepository.create(message);
+    }
 }
